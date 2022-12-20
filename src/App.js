d655e9f06fb0a18335b22c5a8c45d85105cf0827
Harold <harold@pop-os.localdomain>
2022-12-19 19:03:42 -0500

    NavBar Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar/NavBar';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import Contacts from './components/Contacts';
+import Home from './components/Home';
+import Products from './components/Products';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <NavBar/>
+          <Routes>
+            <Route exact path="/" component={<Home />}></Route>
+            <Route exact path="/contacts" component={<Contacts />}></Route>
+            <Route exact path="/products" component={<Products />}></Route>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom';
+// import './navbar.css';
+
+export const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <div class='nav-items'>
+        <NavLink to="/" >
+          <p class='nav-item'>A</p>
+        </NavLink>
+
+        <NavLink to="/b" >
+          <p class='nav-item'>B</p>
+        </NavLink><NavLink to="/c" >
+          <p class='nav-item'>C</p>
+        </NavLink><NavLink to="/d"d >
+          <p class='nav-item'>D</p>
+        </NavLink><NavLink to="/e" >
+          <p class='nav-item'>E</p>
+        </NavLink>
+
+      </div>
+    </div>
+  )
+}
+export default NavBar;
